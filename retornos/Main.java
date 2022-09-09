933b1d61a26ddda03ffd0feed4bc66335d8ea177
Nathan Maria <nathanms2000@outlook.com>
2022-09-09 13:52:02 -0300

    Subindo exercicio sobre retornos

---
+++ retornos/Main.java
@@ -0,0 +1,17 @@
+package BootcampJavaDIO.retornos;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Exercício retornos");
+
+        double areaQuadrado = Quadrilatero.area(3);
+        System.out.println("Área do quadrado:" + areaQuadrado);
+        
+        double areaRetangulo = Quadrilatero.area(5, 5);
+        System.out.println("Área do retangulo:" + areaRetangulo);
+        
+        double areaTrapezio = Quadrilatero.area(7, 8, 9);
+        System.out.println("Área do quadrado:" + areaTrapezio);
+
+    }
+}

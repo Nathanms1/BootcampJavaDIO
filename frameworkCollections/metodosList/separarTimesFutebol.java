4603b2a25f86a998a269f30c6f6d87ae2da36a98
Nathan Maria <nathanms2000@outlook.com>
2022-09-16 18:48:47 -0300

    Subindo V0 de um código para separar 2 times de futebol

---
+++ frameworkCollections/metodosList/separarTimesFutebol.java
@@ -0,0 +1,63 @@
+package BootcampJavaDIO.frameworkCollections.metodosList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class timesFutebol {
+    public static void main(String[] args) {
+        List<Jogador> jogadores = new ArrayList<>(){{
+        add(new Jogador("Nathan", "Goleiro", "Regular"));
+        add(new Jogador("Leo", "Ponta", "Regular"));
+        add(new Jogador("Bruno", "Zagueiro", "Regular"));
+        add(new Jogador("Dudu", "Ala", "Regular"));
+        add(new Jogador("Neymar", "Meia", "Ótimo"));
+        add(new Jogador("Neuer", "Goleiro", "Ótimo"));
+        add(new Jogador("Carlinhos", "Atacante", "Regular"));
+        add(new Jogador("Marco", "Meia", "Regular"));
+
+
+        }};
+        
+        System.out.println(jogadores);
+        
+        
+        Collections.shuffle(jogadores);
+        System.out.println(jogadores);
+    }
+}
+
+class Jogador implements Comparable<Jogador>{
+    private String nome;
+    private String posicao;
+    private String qualidade;
+
+    public Jogador(String nome, String posicao, String qualidade) {
+        this.nome = nome;
+        this.posicao = posicao;
+        this.qualidade = qualidade;
+    }
+
+    public String getNome() {
+        return nome;
+        }
+
+    public String getPosicao() {
+        return posicao;
+        }
+        
+    public String getQualidade() {
+        return qualidade;
+        }
+
+    @Override
+    public String toString() {
+        return "Jogador [Nome= " + nome + ", Posição= " + posicao + ", Nível= " + qualidade + "]";
+    }
+
+    @Override
+    public int compareTo(Jogador player) {
+        
+        return this.getNome().compareToIgnoreCase(player.getNome());
+    }
+}
